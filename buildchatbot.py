67d9d68e45e1fbf50ee59393a3ff4e34f5e1b7b1
Mikk Kirstein <mikk@kolmas.org>
2013-05-14 17:01:22 +0300

    Added support for multiple views

--- buildchatbot.py
+++ buildchatbot.py
@@ -10,14 +10,16 @@ from urllib import urlopen
 from Skype4Py import Skype
 from xml.etree import ElementTree
 
-JENKINS_URL = 'http://127.0.0.1:8080'
-SKYPE_CHAT = '#user/$abc123'
+JENKINS_BASE    = 'http://172.0.0.1:8088'
+JENKINS_VIEW    = [ 'view/here/' ] # A view or list of views to watch
+SKYPE_CHAT      = '#username/$chat'
 UPDATE_INTERVAL = 15  # seconds
-MESSAGE_PREFIX = '[Jenkins] '
+MESSAGE_PREFIX  = '[Jenkins] '
 
 class Build:
   def __init__(self, attrs):
-    self.name = attrs['name']
+    self.name   = attrs['name']
+    self.webUrl = attrs['webUrl']
     self.number = attrs['lastBuildLabel']
     self.status = attrs['lastBuildStatus']
 
@@ -44,6 +46,7 @@ class BuildMonitor:
           self.handle_new_build(build, None)
         elif build.number != self.builds[name].number:
           self.handle_new_build(build, self.builds[name].status)
+
     self.builds = builds
 
   def handle_new_build(self, build, old_status):
@@ -55,13 +58,34 @@ class BuildMonitor:
     elif build.status == 'Failure':
       self.listener.notify(build, '(rain) Failed')
 
+  def fetch_views(self, views):
+    result = []
+    # Loop through jenkins views and fetch the result for each
+    for view in views if not isinstance(views, basestring) else [ views ]:
+      url      = JENKINS_BASE + view + 'cc.xml'
+      response = urlopen(url)
+
+      # Validate status code
+      if (response.code / 100 >= 4):
+        print "Failed to fetch reports from: " + url
+      else:
+        result.append(response);
+
+    return result
+
   def fetch_builds(self):
-    builds = {}
-    response = urlopen(JENKINS_URL +'/cc.xml')
-    projects = ElementTree.parse(response).getroot()
-    for project in projects.iter('Project'):
-      build = Build(project.attrib)
-      builds[build.name] = build
+    builds    = {}
+    views     = self.fetch_views(JENKINS_VIEW)
+
+    # Loop through the view results
+    for response in views:
+      # Build the tree
+      projects  = ElementTree.parse(response).getroot()
+      for project in projects.iter('Project'):
+        build = Build(project.attrib)
+        builds[build.name] = build
+
+    # Return the list of builds
     return builds
 
 class BuildNotifier:
@@ -75,7 +99,7 @@ class BuildNotifier:
     self.chat = skype.Chat(SKYPE_CHAT)
 
   def notify(self, build, event):
-    message = event +': '+ build.name +' - '+ JENKINS_URL +'/job/'+ build.name +'/'+ build.number +'/'
+    message = event +': '+ build.name +' - '+ build.webUrl + build.number
     print message
     self.chat.SendMessage(MESSAGE_PREFIX + message)
 
